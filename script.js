6251e0b70b4438f336aa908d72b96e6337973ad8
Show Wai Yan <showwaiyan555@gmail.com>
2024-05-20 15:21:30 +0630

    Link generateColor funtion with UI

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const colorBlocks = document.querySelectorAll('.color');
+
 // Color Code Generation
 
 function generateColor() {
@@ -5,3 +7,7 @@ function generateColor() {
     for (let i=1; i<=6; i++) colorCode +="123456789ABCDEF"[Math.floor(Math.random()*15)];
     return '#'+colorCode;
 }
+
+colorBlocks.forEach(div=>{
+    div.innerText=div.style.backgroundColor=generateColor();
+});
