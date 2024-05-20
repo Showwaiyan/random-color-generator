47215224ee833d033a2341649092eb34b8a4e4ed
Show Wai Yan <showwaiyan555@gmail.com>
2024-05-20 18:04:35 +0630

    Color copy features finished

--- script.js
+++ script.js
@@ -18,4 +18,13 @@ function divColorAdded() {
 
 divColorAdded(); // Immediate call for color generation
 
+// Color refresh function
 refreshBtn.addEventListener('click',divColorAdded);
+
+// Copy Color function
+colorBlocks.forEach(div=>{
+    div.addEventListener('click',e=>{
+        navigator.clipboard.writeText(div.innerText);
+        alert(`Hex Code ${div.innerText} is copied!!`);
+    })
+});
